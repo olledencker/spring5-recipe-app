e32bec94e8c28999392faee7aa6f7b848d80a27c
olle.dencker <olle.dencker@gmail.com>
2020-08-04 09:50:58 +0200

    Ingredient command and tests

--- src/test/java/guru/springframework/services/IngredientServiceImpTest.java
+++ src/test/java/guru/springframework/services/IngredientServiceImpTest.java
@@ -2,6 +2,7 @@ package guru.springframework.services;
 
 import guru.springframework.commands.IngredientCommand;
 import guru.springframework.converters.IngredientToIngredientCommand;
+import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import guru.springframework.domain.Ingredient;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
@@ -17,36 +18,57 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 public class IngredientServiceImpTest {
-    IngredientService ingredientService;
-
-    @Mock
-    IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
 
     @Mock
     RecipeRepository recipeRepository;
 
-    @Before
-    public void setUp(){
-        MockitoAnnotations.initMocks(this);
-        ingredientService = new IngredientServiceImpl(recipeRepository,ingredientToIngredientCommand);
+    IngredientService ingredientService;
 
+    //init converters
+    public IngredientServiceImpTest() {
+        this.ingredientToIngredientCommand = new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
     }
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        ingredientService = new IngredientServiceImpl(recipeRepository,ingredientToIngredientCommand);
+    }
+
     @Test
-    void findRecipieAndIngredientHappyPath()   {
-        //Given
-        Recipe recipe = Recipe.builder().id(1L).build();
-        recipe.addIngredient(Ingredient.builder().id(1L).build());
-        recipe.addIngredient(Ingredient.builder().id(2L).build());
-        recipe.addIngredient(Ingredient.builder().id(3L).build());
+    public void findByRecipeIdAndId() throws Exception {
+    }
 
+    @Test
+    public void findByRecipeIdAndReceipeIdHappyPath() throws Exception {
+        //given
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        Ingredient ingredient1 = new Ingredient();
+        ingredient1.setId(1L);
+
+        Ingredient ingredient2 = new Ingredient();
+        ingredient2.setId(1L);
+
+        Ingredient ingredient3 = new Ingredient();
+        ingredient3.setId(3L);
+
+        recipe.addIngredient(ingredient1);
+        recipe.addIngredient(ingredient2);
+        recipe.addIngredient(ingredient3);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
+
         when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
         //then
-        IngredientCommand ingredientCommand = ingredientService.findByRecipeIdandIngredientId(1L,3L);
-        assertEquals(ingredientCommand.getId(),Long.valueOf(3L));
-        assertEquals(ingredientCommand.getRecipeId(),Long.valueOf(1L));
-        verify(recipeRepository,times(1)).findById(anyLong());
+        IngredientCommand ingredientCommand = ingredientService.findByRecipeIdandIngredientId(1L, 3L);
 
-
+        //when
+        assertEquals(Long.valueOf(3L), ingredientCommand.getId());
+        assertEquals(Long.valueOf(1L), ingredientCommand.getRecipeId());
+        verify(recipeRepository, times(1)).findById(anyLong());
     }
 }
